209c739919376a62396f4f1e364eceeeb3121bdb
pabfam <rypab4@gmail.com>
2022-11-07 15:16:43 -0800

    changed little code

--- Challenge/script.js
+++ Challenge/script.js
@@ -2,68 +2,74 @@
 var generateBtn = document.querySelector("#generate");
 
 //create prompts
-function generatePassword(){
+function generatePassword() {
   var passLength = parseInt(window.prompt("How long do you want the password? Only 8-128 characters"))
- 
-//if cancels or less than 8 or more than 128
-  if (passLength === null){
-    return; 
-  } 
-  if (isNaN(passLength)){
+
+  //if cancels or less than 8 or more than 128
+  if (!passLength) {
+    alert("Invalid entry. Terminating");
+    return;
+    }
+    
+  if (isNaN(passLength)) {
     window.alert("Invalid entry. Terminating")
     return;
-  
-  } 
-  if (passLength < 8 || passLength > 128){
+
+  }
+  if (passLength < 8 || passLength > 128) {
     window.alert("Invalid length number. Must be between 8-128.")
     return;
   }
-  
-  var numbersPrompt = window.confirm("Do you want numbers in your password?")
-  var symbolsPrompt = window.confirm("Do you want symbols in your password?")
-  var lowerCasePrompt = window.confirm("Do you want lowercase in your password?")
-  var upperCasePrompt = window.confirm("Do you want uppercase in your password?")
+
+  // using confirm prompt
+  var numbersPrompt = window.confirm("Do you want numbers in your password? OK = Yes, Cancel = NO")
+  var symbolsPrompt = window.confirm("Do you want symbols in your password? OK = Yes, Cancel = NO")
+  var lowerCasePrompt = window.confirm("Do you want lowercase in your password? OK = Yes, Cancel = NO")
+  var upperCasePrompt = window.confirm("Do you want uppercase in your password? OK = Yes, Cancel = NO")
   var lowerCase = "abcdefghijklmnopqrztuvwxyz"
-  var upperCase = lowerCase.toUpperCase() 
+  var upperCase = lowerCase.toUpperCase()
   var numbers = "1234567890"
   var symbols = "!@#$%^&*()_+-=<>?:;',./"
-  var lowercaseList= lowerCase.split('')
-  var uppercaseList= upperCase.split('')
+  var lowercaseList = lowerCase.split('')
+  var uppercaseList = upperCase.split('')
   var numbersList = numbers.split('')
   var symbolsList = symbols.split('')
 
   //add array together
   var passArray = []
 
-  if (numbersPrompt === true){
+  // this is for using the confirm prompt
+  if (numbersPrompt === true) {
     var passArray = passArray.concat(numbersList)
   }
-  if (symbolsPrompt === true){
+  if (symbolsPrompt === true) {
     var passArray = passArray.concat(symbolsList)
   }
-  if (upperCasePrompt === true){
+  if (upperCasePrompt === true) {
     var passArray = passArray.concat(uppercaseList)
   }
-  if (lowerCasePrompt === true){
+  if (lowerCasePrompt === true) {
     var passArray = passArray.concat(lowercaseList)
   }
   if (passArray.length === 0) {
     var passArray = passArray.concat(lowercaseList)
   }
-// randomly create password using Math random
-var generatedPassword = []
-  for (var i = 0; i < passLength; i++){
-    var index=(Math.floor(Math.random()*(passArray.length-1)))
+
+
+  // randomly create password using Math random
+  var generatePassword = []
+  for (var i = 0; i < passLength; i++) {
+    var index = (Math.floor(Math.random() * (passArray.length - 1)))
     var pass = passArray[index]
-    generatedPassword.push(pass)
+    generatePassword.push(pass)
   }
   //take away the commas using join
-  generatedPassword = generatedPassword.join('')
+  generatePassword = generatePassword.join('')
 
-  return generatedPassword
+  return generatePassword
 
 }
- 
+
 
 // Write password to the #password input
 function writePassword() {
